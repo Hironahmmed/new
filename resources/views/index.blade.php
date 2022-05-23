7f8a2bac2517fccb58c0f8c086707cf4400c37ac
ebutify <ebutify@gmail.com>
2022-05-23 18:25:21 +0600

    Sitemap.xml Fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,65 +1,71 @@
 <?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>{{ url('/') }}</loc>
+        <lastmod>{{ Carbon\Carbon::now()->toAtomString() }}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.00</priority>
+    </url>
+    <url>
+        <loc>{{ url('/') }}/blog</loc>
+        <lastmod>{{ Carbon\Carbon::now()->toAtomString() }}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.80</priority>
+    </url>
     @foreach ($posts as $post)
         <url>
-            <loc>{{ url('/') }}/post/{{ $post->slug }}</loc>
+            <loc>{{ url('/') }}/blog/post/{{ $post->slug }}</loc>
             <lastmod>{{ $post->created_at->tz('UTC')->toAtomString() }}</lastmod>
             <changefreq>daily</changefreq>
             <priority>0.8</priority>
         </url>
     @endforeach
     <url>
-        <loc>{{ url('/') }}</loc>
-        <lastmod>{{ Carbon\Carbon::now() }}</lastmod>
-        <changefreq>daily</changefreq>
-        <priority>0.8</priority>
-    </url>
-    <url>
-        <loc>{{ url('/') }}/blog</loc>
-        <lastmod>{{ Carbon\Carbon::now() }}</lastmod>
-        <changefreq>daily</changefreq>
-        <priority>0.8</priority>
-    </url>
-    <url>
         <loc>{{ url('/') }}/about</loc>
-        <lastmod>{{ Carbon\Carbon::now() }}</lastmod>
+        <lastmod>{{ Carbon\Carbon::now()->toAtomString() }}</lastmod>
         <changefreq>daily</changefreq>
-        <priority>0.8</priority>
+        <priority>0.80</priority>
     </url>
     <url>
         <loc>{{ url('/') }}/contact-us</loc>
-        <lastmod>{{ Carbon\Carbon::now() }}</lastmod>
+        <lastmod>{{ Carbon\Carbon::now()->toAtomString() }}</lastmod>
         <changefreq>daily</changefreq>
-        <priority>0.8</priority>
+        <priority>0.80</priority>
     </url>
     <url>
         <loc>{{ url('/') }}/affiliate</loc>
-        <lastmod>{{ Carbon\Carbon::now() }}</lastmod>
+        <lastmod>{{ Carbon\Carbon::now()->toAtomString() }}</lastmod>
         <changefreq>daily</changefreq>
-        <priority>0.8</priority>
+        <priority>0.80</priority>
     </url>
     <url>
         <loc>{{ url('/') }}/privacy</loc>
-        <lastmod>{{ Carbon\Carbon::now() }}</lastmod>
+        <lastmod>{{ Carbon\Carbon::now()->toAtomString() }}</lastmod>
         <changefreq>daily</changefreq>
-        <priority>0.8</priority>
+        <priority>0.80</priority>
     </url>
     <url>
         <loc>{{ url('/') }}/terms</loc>
-        <lastmod>{{ Carbon\Carbon::now() }}</lastmod>
+        <lastmod>{{ Carbon\Carbon::now()->toAtomString() }}</lastmod>
         <changefreq>daily</changefreq>
-        <priority>0.8</priority>
+        <priority>0.80</priority>
     </url>
     <url>
         <loc>{{ url('/') }}/faq</loc>
-        <lastmod>{{ Carbon\Carbon::now() }}</lastmod>
+        <lastmod>{{ Carbon\Carbon::now()->toAtomString() }}</lastmod>
         <changefreq>daily</changefreq>
-        <priority>0.8</priority>
+        <priority>0.80</priority>
     </url>
     <url>
         <loc>{{ url('/') }}/login</loc>
-        <lastmod>{{ Carbon\Carbon::now() }}</lastmod>
+        <lastmod>{{ Carbon\Carbon::now()->toAtomString() }}</lastmod>
         <changefreq>daily</changefreq>
-        <priority>0.8</priority>
+        <priority>0.80</priority>
+    </url>
+    <url>
+        <loc>{{ url('/') }}/register</loc>
+        <lastmod>{{ Carbon\Carbon::now()->toAtomString() }}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>0.80</priority>
     </url>
 </urlset>
